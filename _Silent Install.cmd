6111cfa7dec1c55b4dcd29e31a8c8e261e5539fd
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2023-11-12 06:34:13 +0700

    Add files via upload

---
+++ _Silent Install.cmd
@@ -0,0 +1,21 @@
+@ECHO OFF
+@cls
+Color 09
+echo.
+echo.
+echo.
+@echo      ßßßßßßßßßßßßßßßßßßßß  ViSaoThe.Net ßßßßßßßßßßßßßßßßßßßßßßß
+echo.
+echo.
+@echo  Chuong trinh dang cai dat, vui long cho...cua so se tu dong tat ...
+@echo  Chuong trinh chinh sua boi ViSaoThe.Net
+echo.  
+echo.  
+@echo      ßßßßßßßßßßßßßßßßßßßß   ViSaoThe.Net   ßßßßßßßßßßßßßßßßßßßßßßß         
+echo.  
+echo.  
+echo.                             SILENT INSTALLATION...
+echo.                   
+@echo off
+FOR %%i IN ("Nitro*.exe") DO Set FileName="%%i"
+%FileName% /SILENT
